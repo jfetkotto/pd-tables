a712194c98e820931a300a233431f5674c587c23
Sam Mitchell <jfetkotto@gmail.com>
2023-04-01 14:02:35 +0100

    Initial port of C version

---
+++ README.md
@@ -0,0 +1,5 @@
+# pd-tables
+
+`pd-tables -n N -f FILE.pd` generates a puredata file `FILE.pd` containing `N`
+sets of stereo samples named `$0-ID-l` and `$0-ID-r`, the patch has one output
+that provides the abstraction id `$0`.
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use clap::Parser;
+use std::{fs::File, io::Write};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    number_of_tables: u32,
+    #[arg(short, long)]
+    file_name: String,
+}
+
+fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+
+    let x = 10;
+    let mut y = 10;
+    let mut i = 0;
+    let mut f = File::create(args.file_name)?;
+    let mut s: String = String::new();
+
+    s.push_str(&format!("#N canvas 382 105 757 524 10;\n"));
+
+    while i < args.number_of_tables {
+        s.push_str(&format!("#X obj {} {} table \\$0-{}-l;\n", x, y, i));
+        s.push_str(&format!("#X obj {} {} table \\$0-{}-r;\n", 20 * x, y, i));
+        y += 20;
+        i += 1;
+    }
+
+    s.push_str(&format!("#X obj {} {} \\$0;\n", x, y + 30));
+    s.push_str(&format!("#X obj {} {} loadbang;\n", x, y));
+    s.push_str(&format!("#X obj {} {} outlet;\n", x, y + 60));
+    s.push_str(&format!("#X connect {} 0 {} 0;\n", 2 * i, 2 * i + 2));
+    s.push_str(&format!("#X connect {} 0 {} 0;\n", 2 * i + 1, 2 * i));
+    f.write_all(s.as_bytes())?;
+    Ok(())
+}
